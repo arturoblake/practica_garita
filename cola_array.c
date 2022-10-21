a3dd9c9b0e27c88921bed9df7d904b3702c5f0c4
arturoblake <arturo.castanon@uabc.edu.mx>
2022-10-20 19:49:15 -0700

    ahi va

--- cola_array.c
+++ cola_array.c
@@ -3,43 +3,43 @@
 //Prototipos  
 int push(int[100], int, int);  
 int pop(int[100], int);  
-int pilaVacia(int);
-int pilaLLena(int, int);
-void mostrarPila(int[100], int);
+int colaVacia(int);
+int colaLlena(int, int);
+void mostrarCola(int[100], int);
 void top(int[100], int);
 
 void main()  
 {  
     int posicionA = -1;
-    int pila[100];
+    int cola[100];
     int opcion = 0;
     int n;
       
-    printf("\nIngresa el numero de elementos de la pila: ");   
+    printf("\nIngresa el numero de elementos disponibles en la cola ");   
     scanf("%d",&n);    
   
     while(opcion != 6)  
     {  
         printf("\n--------MENU--------\n");  
-        printf("1.Push\n2.Pop\n3.Mostrar la pila\n4.Top\n5.Size\n6.Salir");  
+        printf("1.Push\n2.Pop\n3.Mostrar la cola\n4.Top\n5.Size\n6.Salir");  
         printf("\nIngresa la opcion deseada: ");         
         scanf("%d",&opcion);  
         switch(opcion)  
         {  
             case 1:  
-                posicionA = push(pila, n, posicionA);  
+                posicionA = push(cola, n, posicionA);  
                 break;   
             case 2:   
-                posicionA = pop(pila, posicionA);  
+                posicionA = pop(cola, posicionA);  
                 break;  
             case 3:  
-                mostrarPila(pila, posicionA);  
+                mostrarCola(cola, posicionA);  
                 break;
             case 4:
-                top(pila, posicionA);
+                top(cola, posicionA);
                 break;
             case 5:
-                printf("\nEl tamaño de la pila es: %d",n);//Cuenta el tamano de la pila
+                printf("\nEl tamaño de la cola es: %d",n);//Cuenta el tamano de la cola
                 break;
             case 6:
                 printf("\nTermino el programa.");
@@ -50,29 +50,29 @@ void main()
     } 
     return; 
 }   
-//Permite agregar un valor al principio de la pila  
-int push (int pila[100], int n, int posicionA)  
+//Permite agregar un valor al principio de la cola  
+int push (int cola[100], int n, int posicionA)  
 {  
     int dato = 0;      
-    if (pilaLLena(n, posicionA))
+    if (colaLlena(n, posicionA))
     {   
-        printf("\nLa pila esta llena.\n"); 
+        printf("\nLa cola esta llena.\n"); 
     }
     else   
     {  
         printf("\nIngresa un valor: ");  
         scanf("%d",&dato);         
         posicionA = posicionA +1;   
-        pila[posicionA] = dato;   
+        cola[posicionA] = dato;   
     }
     return posicionA;     
 }   
-//Permite eliminar el elemento al tope de la pila  
-int pop (int pila[100], int posicionA)   
+//Permite eliminar el elemento al tope de la cola  
+int pop (int cola[100], int posicionA)   
 {   
-    if(pilaVacia(posicionA))
+    if(colaVacia(posicionA))
     {   
-        printf("\nLa pila esta vacia.\n");  
+        printf("\nLa cola esta vacia.\n");  
     }
     else
     {  
@@ -80,8 +80,8 @@ int pop (int pila[100], int posicionA)
     }
     return posicionA;     
 }   
-//Valida si hay elementos en la Pila
-int pilaVacia(int posicionA)
+//Valida si hay elementos en la cola
+int colaVacia(int posicionA)
 {
     if(posicionA == -1)
     { 
@@ -92,8 +92,8 @@ int pilaVacia(int posicionA)
         return 0;
     }
 }
-//Verifica si la pila esta llena
-int pilaLLena(int n, int posicionA)
+//Verifica si la cola esta llena
+int colaLlena(int n, int posicionA)
 {
     if(posicionA == n-1)
     { 
@@ -104,30 +104,30 @@ int pilaLLena(int n, int posicionA)
         return 0;
     }
 }
-//Imprime los elementos de la pila
-void mostrarPila(int pila[100], int posicionA)  
+//Imprime los elementos de la cola
+void mostrarCola(int cola[100], int posicionA)  
 {
-    if(pilaVacia(posicionA))   
+    if(colaVacia(posicionA))   
     {  
-        printf("\nLa pila esta vacia.\n");  
+        printf("\nLa cola esta vacia.\n");  
     }    
     else
     {
         for (int i = posicionA; i >= 0; i--)  
         {  
-            printf("[%d]\n",pila[i]);  
+            printf("[%d] ",cola[i]);  
         }  
     }
 }
-//Muestra el elemento del tope de la pila sin eliminarlo
-void top(int pila[100], int posicionA)
+//Muestra el elemento del tope de la cola sin eliminarlo
+void top(int cola[100], int posicionA)
 {
-    if(pilaVacia(posicionA))
+    if(colaVacia(posicionA))
     { 
-        printf("\nPila Vacia");
+        printf("\ncola Vacia");
     }
     else
     { 
-        printf("\n[%d] esta en el top de la pila",pila[posicionA]);
+        printf("\n[%d] esta en el top de la cola",cola[posicionA]);
     }
 }  
