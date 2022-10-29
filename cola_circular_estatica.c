eecf8b05ce39655dbb0c4606acffc9442908dd15
arturoblake <arturo.castanon@uabc.edu.mx>
2022-10-29 01:00:33 -0700

    size

--- cola_circular_estatica.c
+++ cola_circular_estatica.c
@@ -30,7 +30,7 @@ void main()
 				push(cola); 
                 break;   
             case 2:
-
+                pop(cola);
                 break;  
             case 3:
 				mostrarCola(cola);  
@@ -52,37 +52,72 @@ void main()
 
 void push(int cola[MAX])
 {
-    if ((inicio == final + 1) || (inicio == 0 && final == MAX - 1)){
-        printf("La cola esta llena\n");
+    if ((inicio == final + 1) || (inicio == 0 && final == MAX - 1))
+    {
+        printf("\nLa cola esta llena\n");
     }
     else
     {
         int n = 0;
         printf("\nValor n: ");
         scanf("%d", &n);
-        if (inicio == -1){ 
+        if (inicio == -1)
+        { 
             inicio = 0;
         }
         final = (final + 1) % MAX;
         cola[final] = n;
-        printf("%d Entro a la cola.\n", n);
+        printf("\n%d Entro a la cola.", n);
   }
 }
 
 int pop(int cola[MAX])
 {
-
+    int variable;
+    if (inicio == -1) {
+        printf("\nLa cola esta vacia.\n");
+        return -1;
+    } 
+    else
+    {
+        variable = cola[inicio];
+        if (inicio == final) 
+        {
+            inicio = final = -1;
+        } 
+    else 
+    {
+      inicio = (inicio + 1) % MAX;
+    }
+    printf("\n%d Salio de la cola.", variable);
+    return 1;
+  }
 }
 
 void mostrarCola(int cola[MAX])
 {
-    for(int i = final ; i >= inicio; i--)
+    if(inicio == -1)
     {
-        printf("[%d] ", cola[i]);
+        printf("\nLa cola esta vacia.\n");
+    }
+    else
+    {
+        for(int i = final ; i >= inicio; i--)
+        {
+            printf("[%d] ", cola[i]);
+        }
     }
 }
 
-void size(int final){
-    printf("\nLa cantidad de elementos de la cola es: %d ", final +1);
+void size(int final) 
+{
+    if(inicio == -1)
+    {
+        printf("\nLa cola esta vacia.\n");
+    }
+    else
+    {
+        printf("\nLa cantidad de elementos de la cola es: %d ", final +1);
+    }
 }
 
